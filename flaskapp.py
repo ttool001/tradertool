895b486992822aea2a44c4b8dc47d7dba17476b5
yncconsultinginc <yncconsultinginc@gmail.com>
2016-07-13 10:35:10 -0400

    init checkin

--- flaskapp.py
+++ flaskapp.py
@@ -7,14 +7,13 @@ from flask import Flask, request, flash, url_for, redirect, Response, \
      render_template, abort, send_from_directory
 import time
 
-from flask_cache import Cache
+
 app = Flask(__name__)
-# Check Configuring Flask-Cache section for more details
-cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
-dao = Mongodao()
+
 
 
 json.JSONEncoder.default = lambda self,obj: (int(mktime(obj.timetuple())) if isinstance(obj, datetime.datetime) else None)
