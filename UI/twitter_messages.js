4f6a23d9dc64f7e1f2c3412b62f0a580a57a8db3
ddy214 <ddy214@nyu.edu>
2016-07-17 20:31:08 -0400

    change classification function

--- UI/twitter_messages.js
+++ UI/twitter_messages.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	$('#myModal').on('shown.bs.modal', function(){
-		console.log($('#search').val());
+		// console.log($('#search').val());
+		var endpoint = $('#search').val();
+		// $.get
 		//var endpoint = 
 		// $.get(endpoint, function(result){
 
